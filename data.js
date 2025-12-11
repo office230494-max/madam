c8b8decd89c51b68457982024d5bdc31731d823b
office230494-max <office230494@gmail.com>
2025-12-11 12:21:52 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "8312205274:AAGYjiNWLj9wy3cdNDw7z17F-XKq0UuUOiY",
   id:"6620709238",
-  address:"Enter your url"
+  address:"https://replit.com/@office230494/madam"
 };
